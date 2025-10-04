27d246f6f51bcca7244dfd239d21dee7cd7c9fbc
Alper Orus <alperor@gmail.com>
2025-10-04 14:07:15 +0300

    lightbox

--- themes/orus/assets/js/main.js
+++ themes/orus/assets/js/main.js
@@ -48,54 +48,50 @@ document.addEventListener("DOMContentLoaded", () => {
   const lightbox = document.getElementById("lightbox");
   const lightboxImg = document.getElementById("lightbox-img");
   const closeBtn = lightbox.querySelector(".close");
-  const nextBtn = lightbox.querySelector(".next");
-  const prevBtn = lightbox.querySelector(".prev");
 
   const images = Array.from(gallery.querySelectorAll("img"));
   let currentIndex = 0;
-
-function showLightbox(index) {
-  currentIndex = index;
-  lightboxImg.classList.add("fade-out");
-  setTimeout(() => {
-    lightboxImg.src = images[currentIndex].dataset.full;
-    lightboxImg.classList.remove("fade-out");
-  }, 150);
-  lightbox.style.display = "flex";
-}
 
   function closeLightbox() {
     lightbox.style.display = "none";
     lightboxImg.src = "";
   }
 
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  lightboxImg.classList.add("fade-out");
-  setTimeout(() => {
-    lightboxImg.src = images[currentIndex].dataset.full;
-    lightboxImg.classList.remove("fade-out");
-  }, 150);
-}
+  function showLightbox(index) {
+    currentIndex = index;
+    lightboxImg.classList.add("fade-out");
+    setTimeout(() => {
+      lightboxImg.src = images[currentIndex].dataset.full;
+      lightboxImg.classList.remove("fade-out");
+    }, 150);
+    lightbox.style.display = "flex";
+  }
 
-function prevImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  lightboxImg.classList.add("fade-out");
-  setTimeout(() => {
-    lightboxImg.src = images[currentIndex].dataset.full;
-    lightboxImg.classList.remove("fade-out");
-  }, 150);
-}
+  function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    lightboxImg.classList.add("fade-out");
+    setTimeout(() => {
+      lightboxImg.src = images[currentIndex].dataset.full;
+      lightboxImg.classList.remove("fade-out");
+    }, 150);
+  }
 
-// Thumbnail click
+  function prevImage() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.classList.add("fade-out");
+    setTimeout(() => {
+      lightboxImg.src = images[currentIndex].dataset.full;
+      lightboxImg.classList.remove("fade-out");
+    }, 150);
+  }
+
+  // Thumbnail click
   images.forEach((img, i) => {
     img.addEventListener("click", () => showLightbox(i));
   });
 
-  // Lightbox buttons
+  // Close button click
   closeBtn.addEventListener("click", closeLightbox);
-  nextBtn.addEventListener("click", nextImage);
-  prevBtn.addEventListener("click", prevImage);
 
   // Click on image itself -> next
   lightboxImg.addEventListener("click", nextImage);
